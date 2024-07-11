a277f42fed156d8d1885250e22bae50cb2df2351
alexwernick <62384774+alexwernick@users.noreply.github.com>
2024-07-11 10:50:20 +0100

    Delete files (#4)

--- space_people.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-people = requests.get('http://api.open-notify.org/astros.json')
-json = people.json()
-print('Student emails')
-
-for people in json['people']:
-    print(people['name'])
